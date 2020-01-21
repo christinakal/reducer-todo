897195baaf20073f9f4f3d0df65443ca4e7279a5
christina kaliora <develop@pirron.one>
2020-01-22 00:36:59 +0100

    commit1

---
+++ reducer-todo/src/reducers/Reducer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const initialState = {
+    item: "Learn about reducers",
+    completed: false,
+    id: 3892987589
+};
+
+const Reducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+
+export default Reducer;
