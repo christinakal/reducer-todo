2dc16bf7743ff4d7ed50731e96b447ed73d19cf2
christina kaliora <develop@pirron.one>
2020-01-23 00:35:57 +0100

    add todo finished

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useReducer } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import { reducer, initialState } from "./reducers/Reducer";
 
-function App() {
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
+    <div>
+      <TodoList dispatch={dispatch} state={state} />
+      <h2>Add New Tasks Below</h2>
+      <TodoForm dispatch={dispatch} state={state} />
     </div>
   );
-}
+};
 
-export default App; 
+export default App;
